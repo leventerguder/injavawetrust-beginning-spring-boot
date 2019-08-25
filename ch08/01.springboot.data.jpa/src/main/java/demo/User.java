b846292ff57ffc23f211d5f7b72da4b15372e88d
leventerguder <erguder.levent@gmail.com>
2019-08-25 13:51:28 +0300

    chapter 08 spring boot data jpa

---
+++ ch08/01.springboot.data.jpa/src/main/java/demo/User.java
@@ -0,0 +1,54 @@
+package demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	public User() {
+	}
+
+	public User(Integer id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ ch08/01.springboot.data.jpa/src/main/resources/data-mysql.sql
@@ -0,0 +1,3 @@
+insert into users(id, name, email) values(1,'Levent','erguder.levent@injavawetrust.com');
+insert into users(id, name, email) values(2,'Guess','guess@injavawetrust.com');
+insert into users(id, name, email) values(3,'Admin','admin@injavawetrust.com');
