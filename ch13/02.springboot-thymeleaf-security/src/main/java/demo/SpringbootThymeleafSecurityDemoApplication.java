8595d0e215e4eb2336dd2a8eaaddda921b8fdf6a
leventerguder <erguder.levent@gmail.com>
2019-10-05 13:42:40 +0300

    remove

--- ch13/02.springboot-thymeleaf-security/src/main/java/demo/SpringbootThymeleafSecurityDemoApplication.java
+++ ch13/02.springboot-thymeleaf-security/src/main/java/demo/SpringbootThymeleafSecurityDemoApplication.java
@@ -2,7 +2,6 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringbootThymeleafSecurityDemoApplication
